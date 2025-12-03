27ebbbfa4a1feecda113dc2e5f5d352251e51858
Puffer Notebook <marceloballinflores@gmail.com>
2025-12-03 20:27:11 -0300

    teste 2

--- lib/models/tarefa_model.dart
+++ lib/models/tarefa_model.dart
@@ -4,4 +4,12 @@ class Tarefa {
   final String titulo;
 
   Tarefa({required this.id, required this.descricao, required this.titulo});
+
+  factory Tarefa.fromJson(Map<String, dynamic> json) {
+    return Tarefa(
+      id: json['id'],
+      titulo: json['titulo'],
+      descricao: json['descricao'],
+    );
+  }
 }
